b1d40f58e9c57b7babf385fb3be92d6896bf9f98
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-08-15 16:10:08 +0700

    refactor: exclude __v and _id from selecting

--- src/repositories/book-repository.js
+++ src/repositories/book-repository.js
@@ -2,11 +2,11 @@ const Book = require("../models/book-model");
 
 class BookRepository {
   async findAll() {
-    return Book.find({ borrowedBy: null, stock: { $gt: 0 } });
+    return Book.find({ borrowedBy: null, stock: { $gt: 0 } }, "-__v -_id");
   }
 
   async findByCode(code) {
-    return Book.findOne({ code });
+    return Book.findOne({ code }, "-__v -_id");
   }
 }
 
