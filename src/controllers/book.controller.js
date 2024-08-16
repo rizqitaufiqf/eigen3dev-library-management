960bc7ce9444859ecd2f31c403007ae4208edde6
Rizqi Taufiq <rizqitaufiqf@gmail.com>
2024-08-16 15:20:45 +0700

    refactor: small fix

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -4,9 +4,9 @@ const memberService = require("../services/member.service");
 const findAll = async (req, res) => {
   try {
     const books = await bookService.findAll();
-    res.status(200).json(books);
+    return res.status(200).json(books);
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    return res.status(500).json({ error: err.message });
   }
 };
 
@@ -14,12 +14,11 @@ const findByCode = async (req, res) => {
   try {
     const book = await bookService.findByCode(req.params.bookCode);
     if (book) {
-      res.status(200).json(book);
-    } else {
-      res.status(404).json({ message: "Book not found." });
+      return res.status(200).json(book);
     }
+    return res.status(404).json({ message: "Book not found." });
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    return res.status(500).json({ error: err.message });
   }
 };
 
@@ -29,13 +28,12 @@ const borrowBook = async (req, res) => {
     if (!member) throw new Error("Member not found.");
 
     const book = await bookService.borrowBook(member, req.body.bookCode);
-    res.status(200).json({ message: "Book borrowed successfully.", data: book });
+    return res.status(200).json({ message: "Book borrowed successfully.", data: book });
   } catch (err) {
     if (err.message.includes("Book") || err.message.includes("Member")) {
-      res.status(400).json({ message: err.message });
-    } else {
-      res.status(500).json({ message: err.message });
+      return res.status(400).json({ message: err.message });
     }
+    return res.status(500).json({ message: err.message });
   }
 };
 
@@ -45,13 +43,12 @@ const returnBook = async (req, res) => {
     if (!member) throw new Error("Member not found.");
 
     const book = await bookService.returnBook(member, req.body.bookCode);
-    res.status(200).json({ message: "Book returned successfully.", data: book });
+    return res.status(200).json({ message: "Book returned successfully.", data: book });
   } catch (err) {
     if (err.message.includes("Book") || err.message.includes("Member")) {
-      res.status(400).json({ message: err.message });
-    } else {
-      res.status(500).json({ message: err.message });
+      return res.status(400).json({ message: err.message });
     }
+    return res.status(500).json({ message: err.message });
   }
 };
 
